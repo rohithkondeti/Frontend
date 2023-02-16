41662d32c5175b5bfc17311fe506eb33f861e961
Rohith <rohith.kondeti00@gmail.com>
2023-02-16 19:22:54 +0530

    updated 5

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,46 @@
-const Login = () => {
-     return (
-       <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-         <form style={{ width: "30%", padding: "2rem", background: "#f2f2f2", borderRadius: "10px" }}>
-           <h1 style={{ textAlign: "center", marginBottom: "2rem" }}>Login</h1>
-           <div style={{ marginBottom: "1rem" }}>
-             <label htmlFor="username">Username:</label>
-             <input type="text" id="username" name="username" style={{ width: "100%", padding: ".5rem" }} />
-           </div>
-           <div style={{ marginBottom: "1rem" }}>
-             <label htmlFor="password">Password:</label>
-             <input type="password" id="password" name="password" style={{ width: "100%", padding: ".5rem" }} />
-           </div>
-           <button type="submit" style={{ width: "100%", padding: ".5rem", background: "lightblue", border: "none", borderRadius: "5px", color: "#fff" }}>
-             Submit
-           </button>
-         </form>
-       </div>
-     );
-   };
+import React, { useState } from "react";
+import axios from "axios";
 
-   export default Login;
+const Login = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setpassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:8400/auth/authenticate", { userName, password });
+      const token = response.data.token;
+
+      // Store token in local storage
+      localStorage.setItem("token", token);
+
+      // Redirect to dashboard or home page
+      window.location.href = "/product";
+    } catch (err) {
+      console.error(err);
+      alert("Invalid login credentials");
+    }
+  };
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <form style={{ width: "30%", padding: "2rem", background: "#f2f2f2", borderRadius: "10px" }} onSubmit={handleSubmit}>
+        <h1 style={{ textAlign: "center", marginBottom: "2rem" }}>Login</h1>
+        <div style={{ marginBottom: "1rem" }}>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="userName" name="username" style={{ width: "100%", padding: ".5rem" }} onChange={(e) => setUserName(e.target.value)} />
+        </div>
+        <div style={{ marginBottom: "1rem" }}>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" style={{ width: "100%", padding: ".5rem" }} onChange={(e) => setpassword(e.target.value)} />
+        </div>
+        <button type="submit" style={{ width: "100%", padding: ".5rem", background: "lightblue", border: "none", borderRadius: "5px", color: "#fff" }}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
