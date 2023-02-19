754d8e2f255867897b100090e1fec4e1fc3a0a2f
Rohith <rohith.kondeti00@gmail.com>
2023-02-19 19:16:11 +0530

    updated

--- src/components/Name.js
+++ src/components/Name.js
@@ -1,64 +1,84 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const ProductName = () => {
   const [productName, setProductName] = useState('');
   const [productData, setProductData] = useState(null);
+  const [error, setError] = useState('');
 
-  const handleSubmit = async(event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    let name = event.target.elements.productName.value;
-    console.log(name);
+    const name = event.target.elements.productName.value;
     try {
       const response = await axios.get(`http://localhost:8081/product/productname/${name}`, {
         headers: { 'Access-Control-Allow-Origin': '*' },
-        proxy: { 
+        proxy: {
           host: 'http://cors-anywhere.herokuapp.com/',
-          port: 80
-        }
+          port: 80,
+        },
       });
       setProductData(response.data);
-      console.log(response.data);
+      setError('');
     } catch (error) {
       console.error(error);
+      setProductData(null);
+      setError('Product not found!');
     }
   };
 
   return (
-    (!productData) ? (
-      <div className="dialog-container">
-        <div className="dialog-header">
-          <h3>Product Name</h3>
-          <button className="close-btn">x</button>
+    <div className="product-name-container">
+      <h1 className="product-name-header"> </h1>
+      <form onSubmit={handleSubmit} className="product-name-form">
+        <label htmlFor="product-name" className="product-name-label">
+          Enter Product Name:
+        </label>
+        <div className="product-name-input-wrapper">
+          <input
+            type="text"
+            id="product-name"
+            name="productName"
+            className="product-name-input"
+            placeholder="E.g. Product Name"
+            value={productName}
+            onChange={(event) => setProductName(event.target.value)}
+          />
+          <button type="submit" className="product-name-submit">
+            Lookup
+          </button>
         </div>
-        <div className="dialog-body">
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="product-name">Enter Product Name:</label>
-              <input
-                type="text"
-                id="product-name"
-                name='productName'
-                className="form-control"
-                placeholder="Enter Product Name"
-                value={productName}
-                onChange={(event) => setProductName(event.target.value)}
-              />
-            </div>
-            <button className="submit-btn" type="submit">Submit</button>
-          </form> 
+        {error && <p className="product-name-error">{error}</p>}
+      </form>
+      {productData && (
+        <div className="product-details">
+          <h2 className="product-details-header">Product Details</h2>
+          <table className="product-details-table">
+            <tbody>
+              <tr>
+                <td className="product-details-label">Product Name:</td>
+                <td className="product-details-value">{productData.productName}</td>
+              </tr>
+              <tr>
+                <td className="product-details-label">Company Name:</td>
+                <td className="product-details-value">{productData.companyName}</td>
+              </tr>
+              <tr>
+                <td className="product-details-label">Category:</td>
+                <td className="product-details-value">{productData.category}</td>
+              </tr>
+              <tr>
+                <td className="product-details-label">Price:</td>
+                <td className="product-details-value">${productData.price.toFixed(2)}</td>
+              </tr>
+              <tr>
+                <td className="product-details-label">Rating:</td>
+                <td className="product-details-value">{productData.rating.toFixed(1)}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-      </div>
-    ) : (
-      <div className="product-details">
-        <h3>Product Details</h3>
-        <p><b>Product Name:</b> {productData.productName}</p>
-        <p><b>Company Name:</b> {productData.companyName}</p>
-        <p><b>Category:</b> {productData.category}</p>
-        <p><b>Price:</b> {productData.price}</p>
-        <p><b>Rating:</b> {productData.rating}</p>
-      </div>
-    )
+      )}
+    </div>
   );
 };
 
--- src/components/ProductId.js
+++ src/components/ProductId.js
@@ -1,65 +1,89 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+
 
 const ProductId = () => {
   const [productId, setProductId] = useState('');
   const [productData, setProductData] = useState(null);
+  const [error, setError] = useState('');
 
-  const handleSubmit = async(event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    let pid = event.target.elements.productId.value
-    console.log(pid)
+    const pid = event.target.elements.productId.value;
     try {
       const response = await axios.get(`http://localhost:8081/product/product/${pid}`, {
         headers: { 'Access-Control-Allow-Origin': '*' },
-        proxy: { 
+        proxy: {
           host: 'http://cors-anywhere.herokuapp.com/',
-          port: 80
-        }
+          port: 80,
+        },
       });
       setProductData(response.data);
-      console.log(response.data);
+      setError('');
     } catch (error) {
       console.error(error);
+      setProductData(null);
+      setError('Product not found!');
     }
   };
 
   return (
-    (!productData) ? (
-      <div className="dialog-container">
-        <div className="dialog-header">
-          <h3>Product ID</h3>
-          <button className="close-btn">x</button>
+    <div className="product-id-container">
+      <h1 className="product-id-header"> </h1>
+      <form onSubmit={handleSubmit} className="product-id-form">
+        <label htmlFor="product-id" className="product-id-label">
+          Enter Product ID:
+        </label>
+        <div className="product-id-input-wrapper">
+          <input
+            type="text"
+            id="product-id"
+            name="productId"
+            className="product-id-input"
+            placeholder="E.g. 12345"
+            value={productId}
+            onChange={(event) => setProductId(event.target.value)}
+          />
+          <button type="submit" className="product-id-submit">
+            Lookup
+          </button>
         </div>
-        <div className="dialog-body">
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="product-id">Enter Product Id:</label>
-              <input
-                type="text"
-                id="product-id"
-                name='productId'
-                className="form-control"
-                placeholder="Enter Product Id"
-                value={productId}
-                onChange={(event) => setProductId(event.target.value)}
-              />
-            </div>
-            <button className="submit-btn" type="submit">Submit</button>
-          </form> 
+        {error && <p className="product-id-error">{error}</p>}
+      </form>
+      {productData && (
+        <div className="product-details">
+          <h2 className="product-details-header">Product Details</h2>
+          <table className="product-details-table">
+            <tbody>
+              <tr>
+                <td className="product-details-label">Product ID:</td>
+                <td className="product-details-value">{productData.id}</td>
+              </tr>
+              <tr>
+                <td className="product-details-label">Product Name:</td>
+                <td className="product-details-value">{productData.productName}</td>
+              </tr>
+              <tr>
+                <td className="product-details-label">Company Name:</td>
+                <td className="product-details-value">{productData.companyName}</td>
+              </tr>
+              <tr>
+                <td className="product-details-label">Category:</td>
+                <td className="product-details-value">{productData.category}</td>
+              </tr>
+              <tr>
+                <td className="product-details-label">Price:</td>
+                <td className="product-details-value">${productData.price.toFixed(2)}</td>
+              </tr>
+              <tr>
+                <td className="product-details-label">Rating:</td>
+                <td className="product-details-value">{productData.rating.toFixed(1)}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-      </div>
-    ) : (
-      <div className="product-details">
-        <h3>Product Details</h3>
-        <p><b>Product Id:</b> {productData.id}</p>
-        <p><b>Product Name:</b> {productData.productName}</p>
-        <p><b>Company Name:</b> {productData.companyName}</p>
-        <p><b>Category:</b> {productData.category}</p>
-        <p><b>Price:</b> {productData.price}</p>
-        <p><b>Rating:</b> {productData.rating}</p>
-      </div>
-    )
+      )}
+    </div>
   );
 };
 
