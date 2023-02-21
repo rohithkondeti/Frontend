c95996525e716533bfa8710b8db03be1204b5650
Rohith <rohith.kondeti00@gmail.com>
2023-02-21 12:58:24 +0530

    updated

--- src/components/All.js
+++ src/components/All.js
@@ -24,29 +24,32 @@ const All = () => {
   }, []);
 
   return (
-    (!data2) ? (
-      <div style={{textAlign: 'center', padding: '20px'}}>Loading...</div>
-    ) : (
-      <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center'}}>
-        {data2.map(product => (
-          <div style={{width: '400px', height: '200px', background: '#f2f2f2', margin: '20px', textAlign: 'center', padding: '20px'}}>
-            <h2 style={{color: '#0000ff'}}>{product.productName}</h2>
-            <p><b>Company Name:</b> {product.companyName}</p>
-            <p><b>Category:</b> {product.category}</p>
-            <p><b>Price:</b> {product.price}</p>
-            <p><b>Rating:</b> {product.rating}</p>
-            <div style={{display: 'flex', justifyContent: 'center', marginTop: '10px'}}>
-              <Link to="/addfeedback">
-                <button style={{margin: '5px'}} >Add Feedback</button>
+    <div>
+      
+      {!data2 ? (
+        <div style={{textAlign: 'center', padding: '20px'}}>Loading...</div>
+      ) : (
+        <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center'}}>
+          {data2.map(product => (
+            <div style={{width: '400px', height: '200px', background: '#f2f2f2', margin: '20px', textAlign: 'center', padding: '20px'}}>
+              <h2 style={{color: '#0000ff'}}>{product.productName}</h2>
+              <p><b>Company Name:</b> {product.companyName}</p>
+              <p><b>Category:</b> {product.category}</p>
+              <p><b>Price:</b> {product.price}</p>
+              <p><b>Rating:</b> {product.rating}</p>
+              <div style={{display: 'flex', justifyContent: 'center', marginTop: '10px'}}>
+                <Link to={`/addfeedback?productId=${product.id}`}>
+                  <button style={{margin: '5px'}}>Add Feedback</button>
                 </Link>
                 <Link to="/getfeedback">
-                <button style={{margin: '5px'}}>View Feedback</button>
+                  <button style={{margin: '5px'}}>View Feedback</button>
                 </Link>
               </div>
-          </div>
-        ))}
-      </div>
-    )
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   );
 };
 
