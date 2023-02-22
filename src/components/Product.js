04aca35f9f0d3f93f24c8b8fb82f7598439cb802
Rohith <rohith.kondeti00@gmail.com>
2023-02-22 16:32:52 +0530

    Added product

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,7 +7,7 @@ const Product = () => {
   return (
     <>
       <Navbar1 />
-      <section className="hero-section" style={{backgroundImage: "url(https://img.freepik.com/free-photo/magnificent-woman-long-bright-skirt-dancing-studio-carefree-inspired-female-model-posing-with-pleasure-yellow_197531-11084.jpg?w=1060&t=st=1676976656~exp=1676977256~hmac=5081fe406cac2199638da9b525f9a30784a4a15398413b95b96ba88d5d112798)", backgroundSize: "cover", backgroundPosition: "center", height: "100vh"}}>
+      <section className="hero-section" style={{backgroundImage: "url(https://github.com/rohithkondeti/Frontend/blob/main/src/image/magnificent-woman-long-bright-skirt-dancing-studio-carefree-inspired-female-model-posing-with-pleasure-yellow.jpg?raw=true)", backgroundSize: "cover", backgroundPosition: "center", height: "100vh"}}>
         <p>Welcome!</p>
         </section>
     </>
